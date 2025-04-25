2dcd99955363bf61735d80f4b475ed13eec50cb6
Elainelyy <liaoyythu@gmail.com>
2025-04-24 22:33:58 -0700

    Add logoutParams

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export default function LoginButton() {
-  const { loginWithRedirect, logout, isAuthenticated, user, isLoading } = useAuth0();
+    const { loginWithRedirect, logout, isAuthenticated, user, isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <div className="loading-button">Loading...</div>;
-  }
+    if (isLoading) {
+        return <div className="loading-button">Loading...</div>;
+    }
 
-  if (isAuthenticated) {
+    if (isAuthenticated) {
+        return (
+            <div className="auth-container">
+                <span className="user-info">
+                    {user.name}
+                </span>
+                <button
+                    className="logout-button"
+                    onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+                >
+                    Log Out
+                </button>
+            </div >
+        );
+    }
+
     return (
-      <div className="auth-container">
-        <span className="user-info">
-          {user.name}
-        </span>
         <button
-          className="logout-button"
-          onClick={() => logout({ returnTo: window.location.origin })}
+            className="login-button"
+            onClick={() => loginWithRedirect()}
         >
-          Log Out
+            Log In
         </button>
-      </div>
     );
-  }
-
-  return (
-    <button
-      className="login-button"
-      onClick={() => loginWithRedirect()}
-    >
-      Log In
-    </button>
-  );
 } 
